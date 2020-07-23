a4527d0f3ab6c8d3ce747f2df2a56207ba09283c
Jiangtao Liu <jiangtao.liu@sas.com>
2020-07-23 04:01:08 -0400

    After upload the file to Gighub, made another change locally.

--- test1.c
+++ test1.c
@@ -2,6 +2,7 @@ int main()
 {
   /* change some thing in main branch. */
   /* fixbug2 change. */
+  printf("After upload the file to Gighub, made another change locally.");
   return 0;
 }
 /*Wed Jul 22 05:42:39 EDT 2020*/
