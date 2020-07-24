af46c6eef5f599b2204b5ebf5c319a1e2566e4ea
Jiangtao Liu <jiangtao.liu@sas.com>
2020-07-24 01:40:24 -0400

    made some change in master.

--- test1.c
+++ test1.c
@@ -4,6 +4,7 @@ int main()
   /* fixbug2 change. */
   printf("After upload the file to Gighub, made another change locally.");
   printf("fixbug3 change.");
+  printf("Make a conflict change in main branch.");
   return 0;
 }
 /*Wed Jul 22 05:42:39 EDT 2020*/
